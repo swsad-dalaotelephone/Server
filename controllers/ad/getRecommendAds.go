1942123107d3560556c804c4fb3997646d38be09
gitgiter <1176748429@qq.com>
2019-05-29 03:33:42 -0400

    implement ad controller, commit before merge

--- controllers/ad/getRecommendAds.go
+++ controllers/ad/getRecommendAds.go
@@ -1,6 +1,12 @@
 package adController
 
 import (
+	"github.com/swsad-dalaotelephone/Server/models/ad"
+	"github.com/swsad-dalaotelephone/Server/modules/log"
+	"github.com/swsad-dalaotelephone/Server/modules/util"
+	"errors"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,4 +17,23 @@ return:
 */
 func GetRecommendAds(c *gin.Context) {
 
+	// get all ads	
+	ads, err := adModel.GetAllAds()	
+
+	if err != nil {
+		log.ErrorLog.Println(err)	
+		return	
+	}
+
+	if len(ads) > 0 {
+		c.JSON(http.StatusOK, gin.H{
+			"ads": ads
+		})		
+		log.InfoLog.Println(id, len(ads), "success")
+	} else {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": "can not fetch ad list"
+		})
+		log.ErrorLog.Println("can not fetch ad list")
+	}
 }
