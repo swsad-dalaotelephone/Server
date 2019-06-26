336633d7ad1b4c8a75bf3dbcaa26a37e38eefbd7
gitgiter <1176748429@qq.com>
2019-06-27 00:17:24 +0800

    debug

--- controllers/task/getStatistics.go
+++ controllers/task/getStatistics.go
@@ -20,7 +20,7 @@ type ResultItem struct {
 }
 
 type result struct {
-	statistics []ResultItem
+	Statistics []ResultItem `json:"statistics"`
 }
 
 /*
@@ -190,9 +190,9 @@ func GetStatistics(c *gin.Context) {
 	}
 
 	log.ErrorLog.Println("res")
-	log.ErrorLog.Println(res.statistics)
+	log.ErrorLog.Println(res)
 
-	resJson, err := util.StructToJson(res.statistics)
+	resJson, err := util.StructToJsonStr(res)
 
 	log.ErrorLog.Println("resJson")
 	log.ErrorLog.Println(resJson)
@@ -206,9 +206,8 @@ func GetStatistics(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"statistics": resJson,
-	})
+	c.JSON(http.StatusOK, resJson)
+
 	log.InfoLog.Println(len(res.statistics), "success")
 
 }
